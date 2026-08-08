61b9f54a62b7457ba21fb4dd712981ed33311e63
ejeraai <ezrarahmadityaa@gmail.com>
2026-08-08 09:58:09 +0700

    fix gmaps halaman contact

--- public/js/app.js
+++ public/js/app.js
@@ -1547,8 +1547,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 scrollWheelZoom: false,
             }).setView([data.lat, data.lng], 16);
 
-            L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener">OpenStreetMap</a> contributors',
+            L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions" target="_blank" rel="noopener">CARTO</a>',
+                subdomains: "abcd",
                 maxZoom: 19,
             }).addTo(map);
 
